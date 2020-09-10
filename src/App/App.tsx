0e852d2f20f58bf1b1ad0acd9f51811b230946c8
Astroner <zhenyanikforov@gmail.com>
2020-09-10 23:17:41 +0300

    maeterial-ui

--- src/App/App.tsx
+++ src/App/App.tsx
@@ -1,5 +1,7 @@
 import React, { FC, memo } from 'react';
 import { Router, Route } from "react-router-dom";
+import { IconButton, AppBar, Toolbar } from "@material-ui/core"
+import MenuIcon from "@material-ui/icons/Menu"
 
 import { history } from "@/helpers/history"
 import * as Routs from "@/routs";
@@ -11,9 +13,18 @@ export interface IApp {
 
 const App: FC<IApp> = props => {
     return (
-		<Router history={history}>
-			<Route path={Routs.HOME} component={HomePage} />
-		</Router>
+		<>
+			<AppBar position="fixed">
+				<Toolbar>
+					<IconButton color="inherit">
+						<MenuIcon />
+					</IconButton>
+				</Toolbar>
+			</AppBar>
+			<Router history={history}>
+				<Route path={Routs.HOME} component={HomePage} />
+			</Router>
+		</>
 	)
 }
 
