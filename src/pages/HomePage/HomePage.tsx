29e5cb0336afcff4efc69a7aee7c7cc7a4837a78
Molly <holymolestianinqiusition@gmail.com>
2020-09-18 15:21:37 -0700

    QFix σ v0.4

--- src/pages/HomePage/HomePage.tsx
+++ src/pages/HomePage/HomePage.tsx
@@ -2,6 +2,7 @@ import React, { FC, memo } from "react"
 import PageLayout from "@/layouts/PageLayout"
 import { useTranslation } from "react-i18next"
 import usePosts from "./usePosts"
+import { Grid, Paper } from "@material-ui/core"
 
 export interface IHomePage {}
 
@@ -13,9 +14,24 @@ const HomePage: FC<IHomePage> = () => {
 		<PageLayout title={"Home page"}>
 			{t("homePage.title")}
 			<div>{loading && "Loading..."}</div>
-			{posts.map((item) => (
-				<div key={item.id}>{item.title}</div>
-			))}
+			<Grid container spacing={1} style={{ paddingTop: "20px" }}>
+				{posts.map((item) => (
+					<Grid item xs={4} key={item.id} direction={"column"}>
+						<Paper elevation={2}>
+							<div
+								style={{
+									paddingBottom: "20px",
+									wordWrap: "normal",
+									textOverflow: "hidden",
+								}}
+							>
+								{item.title}
+							</div>
+							<div>{item.text}</div>
+						</Paper>
+					</Grid>
+				))}
+			</Grid>
 		</PageLayout>
 	)
 }
