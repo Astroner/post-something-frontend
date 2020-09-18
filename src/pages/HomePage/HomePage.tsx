409c0321e1126179a552342e3c89a2cc1a4c4e4a
Molly <holymolestianinqiusition@gmail.com>
2020-09-18 15:50:52 -0700

    QFix σ v0.5

--- src/pages/HomePage/HomePage.tsx
+++ src/pages/HomePage/HomePage.tsx
@@ -17,17 +17,26 @@ const HomePage: FC<IHomePage> = () => {
 			<Grid container spacing={1} style={{ paddingTop: "20px" }}>
 				{posts.map((item) => (
 					<Grid item xs={4} key={item.id} direction={"column"}>
-						<Paper elevation={2}>
+						<Paper elevation={2} style={{ padding: "15px 20px " }}>
+							<div>
+								<img
+									src={item.displayImage}
+									style={{ width: "100%" }}
+									alt="Mamu ebal"
+								></img>
+							</div>
 							<div
 								style={{
+									width: "100%",
 									paddingBottom: "20px",
-									wordWrap: "normal",
-									textOverflow: "hidden",
+									whiteSpace: "nowrap",
+									overflow: "hidden",
+									textOverflow: "ellipsis",
 								}}
 							>
 								{item.title}
 							</div>
-							<div>{item.text}</div>
+							<div>{item.shortText}</div>
 						</Paper>
 					</Grid>
 				))}
